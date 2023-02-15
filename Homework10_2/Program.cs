2a0e274baa7ce3ad2ec25a620e1579fee2bf3df7
kostenkoi24 <kostenko.i@gmail.com>
2023-02-15 19:50:54 +0200

    Homework10

--- Homework10_2/Program.cs
+++ Homework10_2/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Homework10_2
 {
+
+    public class Base
+    {
+        public void DoWork() // Шаблонний метод
+        {
+            PreDoWork();
+            CoreDoWork();
+        }
+
+        protected virtual void PreDoWork() { Console.WriteLine("0"); }
+        protected virtual void CoreDoWork() { Console.WriteLine("1"); } //будет замещён методом из Derived класса
+    }
+
+    public class Derived : Base
+    {
+        protected override void CoreDoWork() { Console.WriteLine("2"); }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Base instance = new Derived(); 
+            instance.DoWork();  // Console.WriteLine("2")
+
+            // Delay
+            Console.ReadKey();
         }
     }
 }
---
+++ Homework10_4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework10_4
+{
+
+    public abstract class Animal
+    {
+        public void Speak()
+        {
+            GetSound();
+        }
+        protected abstract void GetSound();
+     
+    }
+
+    class Dog : Animal
+    {
+        protected override void GetSound()
+        {
+            Console.WriteLine("Gav-Gav") ;
+        }
+    }
+    class Cow : Animal
+    {
+        protected override void GetSound()
+        {
+            Console.WriteLine("Mu-Muuu");
+        }
+    }
+
+    class Program
+    {
+        /*Реалізуйте шаблон NVI у власній ієрархії успадкування.*/
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Speak();
+
+            Cow cow = new Cow();
+            cow.Speak();
+            Console.ReadKey();
+        }
+    }
+}
